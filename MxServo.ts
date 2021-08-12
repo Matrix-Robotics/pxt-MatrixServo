2dbdc0bf15cc85b292b99bd747b88c99b5f2d9b9
DCisHurt <46733725+DCisHurt@users.noreply.github.com>
2021-08-13 01:14:48 +0800

    Add files via upload

---
+++ MxServo.ts
@@ -0,0 +1,94 @@
+let __RLS = 0
+
+enum ServoChannel
+{
+    CH1 = 1,
+    CH2,
+    CH3,
+    CH4,
+    CH5,
+    CH6,
+    CH7,
+    CH8
+}
+
+//% weight=3 Servo=#8022be icon="f0b2" block="MxServo"
+namespace MxServo{
+
+    const MxServo_ADDR = 0x25
+
+    enum ServoReg
+    {
+        Device_ID = 1,
+        Device_Control_1,
+        Device_Control_2,
+        Battery_Voltage,
+    }
+
+    /**
+     *start up the servo extension
+    */
+    //%block="start up the servo extension"
+    //%weight=994 inlineInputMode="external" %blockID="MxServo_init"
+    export function init(): void {
+
+        if(i2cRead(ServoReg.Device_ID) == 0x46){
+            i2cWrite(ServoReg.Device_Control_1, 0x08); // reset
+            basic.pause(500);
+            i2cWrite(ServoReg.Device_Control_1, 0x00); // enable
+        }
+    }
+
+    /**
+     *set servo angle
+     *@param ch [1-8] select the channel of servo; eg: 1, 6
+     *@param angle [0-180] set the angle of servo; eg: 0, 180
+    */
+    //%block="set %ch servo to %angle"
+    //%weight=94 %blockID="MxServo_Angle"
+    //% angle.min=0 angle.max=180
+    export function setAngle(ch: ServoChannel, angle: number): void {
+        let shift = 1 << (8-ch)
+        __RLS |= shift
+    
+        i2cWrite(ServoReg.Device_Control_2, __RLS)
+
+        i2cWrite(ch+4, angle)
+    }
+
+    /**
+     *release the channel of servo
+     *@param ch [1-8] select the channel of servo; eg: 1, 6
+     *@param state logic of release; eg: true, false
+    */
+    //%block="servo %ch release"
+    //%weight=93 %blockID="MxServo_Angle"
+    export function ChannelRelease(ch: ServoChannel): void {
+        let shift = 1 << (8-ch)
+        let mask = 0x0F - shift
+        __RLS &= mask
+
+        i2cWrite(ServoReg.Device_Control_2, __RLS)
+    }
+
+    /**
+     *read voltage of servo extension
+    */
+    //%block="read voltage of servo extension"
+    //%weight=92 %blockID="MxServo_Vbat"
+    export function readVbat(): number{
+        return i2cRead(ServoReg.Battery_Voltage)*0.033
+    }
+
+    function i2cWrite(reg: number, value: number): void {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(MxServo_ADDR, buf)
+    }  
+    
+    function i2cRead(reg: number): number {
+        pins.i2cWriteNumber(MxServo_ADDR, reg, NumberFormat.UInt8LE)
+        return pins.i2cReadNumber(MxServo_ADDR, NumberFormat.UInt8LE, false)
+    } 
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+## Introduction
+
+This library provides a driver for Matrix Servo Extension, see [here](https://matrixrobotics.com/) for further information!
+
+## Basic usage
+
+* Set Motor to 100% speed
+
+```blocks
+    Matrix.motor(M_PORT.M1, 100);
+    Matrix.motor(M_PORT.M2, 100);
+```
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* Open [Microsoft MakeCode for micro:bit](https://makecode.microbit.org/)
+* Click on **New Project**
+* Click on **Extensions** under the gearwheel menu
+* Search for **https://github.com/Matrix-Robotics/pxt-MatrixServo/** and import
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+
+```package
+MatrixServo=github:Matrix-Robotics/pxt-MatrixServo
+```
