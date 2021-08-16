662aa718bdb35bbff19f36437df533b168bdd0f7
DCisHurt <roger33669@gmail.com>
2021-08-16 10:42:45 +0800

    update test file

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-
+MxServo.init()
+basic.forever(function () {
+    serial.writeString("Battery=")
+    serial.writeLine("" + (MxServo.readVbat()))
+    MxServo.setAngle(ServoChannel.CH4, 0)
+    MxServo.setAngle(ServoChannel.CH8, 180)
+})
